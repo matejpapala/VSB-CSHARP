e692119bef7759ef1de3234199de69e827698074
matejpapala <matejpapala1234@gmail.com>
2025-09-30 10:18:34 +0200

    nullables, enums

--- cviceni1/obj/Debug/net9.0/cviceni1.AssemblyInfo.cs
+++ cviceni1/obj/Debug/net9.0/cviceni1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cviceni1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c73932b03622f0b7b4fcc7580f011b2c2ddce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d827315bd5b89e4e3057003ddcc7ff44306ef6e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("cviceni1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cviceni1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ cviceni3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Globalization;
+
+namespace cviceni3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo ci = CultureInfo.GetCultureInfo("cs-CZ");
+        CultureInfo ciUs = CultureInfo.GetCultureInfo("en-US");
+        CultureInfo ciGb = CultureInfo.GetCultureInfo("en-GB");
+
+
+        Console.WriteLine("Zadejte cislo:");
+        string? input = Console.ReadLine();
+
+        int? x = ParseIntOrNull(input!);
+        // int num;
+        if(TryParseInt(input!, out int num, ParseIntOptionEnum.ALLOW_WHITESPACES | ParseIntOptionEnum.ALLOW_NEGATIVE_NUMBERS)) {
+            Console.WriteLine("Neplatny vstup");
+        } else {
+            Console.WriteLine($"Vstup je platne cislo: {num}");
+        }
+    }
+
+    public static int ParseInt(string input) {
+        int result = 0;
+        if(input == null) {
+            return -1;
+        }
+        for(int i = 0;i < input.Length;i++) {
+            if(input[i] < '0' || input[i] > '9') {
+                return -1;
+            }
+
+            result = input[i] - '0' + result * 10;
+        }
+        return result;
+    }
+
+    static int? ParseIntOrNull(string input) {
+        int result = 0;
+        if(input == null) {
+            return null;
+        }
+        for(int i = 0;i < input.Length;i++) {
+            if(input[i] < '0' || input[i] > '9') {
+                return null;
+            }
+
+            result = input[i] - '0' + result * 10;
+        }
+        return result;
+    }
+
+    static bool TryParseInt(string input, out int result) {
+        result = 0;
+        if(input == null) {
+            return false;
+        }
+        for(int i = 0;i < input.Length;i++) {
+            if(input[i] < '0' || input[i] > '9') {
+                return false;
+            }
+
+            result = input[i] - '0' + result * 10;
+        }
+        return true;
+    }
+
+    static bool TryParseInt(string input, out int result, ParseIntOptionEnum options) {
+        result = 0;
+        bool isNegative = false;
+        if(input == null) {
+            return false;
+        }
+        for(int i = 0;i < input.Length;i++) {
+            if(input[i] < '0' || input[i] > '9') {
+                if(i == 0 && input[i] == '-') {
+                    isNegative = true;
+                    continue;
+                }
+                if(options.HasFlag(ParseIntOptionEnum.IGNORE_INVALID_CHARACTERS)) continue;
+                if(options.HasFlag(ParseIntOptionEnum.ALLOW_WHITESPACES) && char.IsWhiteSpace(input[i])) continue;
+                return false;
+            }else {
+                result = input[i] - '0' + result * 10;
+            }
+            
+        }
+        if(isNegative) result = -result;
+        return true;
+    }
+
+    enum ParseIntOptionEnum {
+        NONE = 1,
+        ALLOW_WHITESPACES = 2,
+        ALLOW_NEGATIVE_NUMBERS = 4,
+        IGNORE_INVALID_CHARACTERS = 8
+    }
+}
